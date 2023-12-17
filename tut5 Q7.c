bbfb4178560f5c97108bc03b1e8dfc304da0b04d
Pramudi Dulasha <148691504+KHPDULASHA@users.noreply.github.com>
2023-12-17 21:58:58 +0530

    Add files via upload
    tutorial 5 Q7

---
+++ tut5 Q7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+   float x;
+   int y;
+
+   printf("Enter the value for x:");
+   scanf("%f",&x);
+
+   if (x<0)
+   {
+       printf("y is: %d",y=1);
+
+   }
+   else if (x==0)
+   {
+        printf("y is: %d",y=0);
+   }
+   else if (x>0)
+   {
+        printf("y is: %d",y=-1);
+   }
+    return ;
+
+
+
+
+
+
+}
